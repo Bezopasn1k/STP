dc8913f1b2c6e58a0a1896ce5bd71dfa487dd150
Влад Петровский <petrovskiy.ve@yandex.ru>
2024-03-10 13:17:26 +0700

    Complete working on RGZ

---
+++ STP PART 2/RGZ_Petrovskiy/rgz/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace rgz
+{
+    public partial class Form1 : Form
+    {
+        ADT_Control<TFrac, TEditor> fracController;
+
+        const string operation_signs = "+-/*";
+        string memmory_buffer = string.Empty;
+
+        public Form1()
+        {
+            fracController = new ADT_Control<TFrac, TEditor>();
+            InitializeComponent();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            memmory_buffer = textBox1.Text;
+            MessageBox.Show("Скопировано в буфер обмена - " + memmory_buffer,
+                    "Информация",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        private void EnterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (memmory_buffer == string.Empty)
+            {
+                MessageBox.Show("Буфер обмена пуст.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+            textBox1.Text = memmory_buffer;
+        }
+
+        private static int CharToEditorCommand(char ch)
+        {
+            int command = 66;
+            switch (ch)
+            {
+                case '0':
+                    command = 0;
+                    break;
+                case '1':
+                    command = 1;
+                    break;
+                case '2':
+                    command = 2;
+                    break;
+                case '3':
+                    command = 3;
+                    break;
+                case '4':
+                    command = 4;
+                    break;
+                case '5':
+                    command = 5;
+                    break;
+                case '6':
+                    command = 6;
+                    break;
+                case '7':
+                    command = 7;
+                    break;
+                case '8':
+                    command = 8;
+                    break;
+                case '9':
+                    command = 9;
+                    break;
+                case '.':
+                    command = 10;
+                    break;
+                case '-':
+                    command = 11;
+                    break;
+            }
+
+            return command;
+        }
+        private static int CharToOperationsCommand<T>(char ch) where T : TFrac, new()
+        {
+            int command = 0;
+            switch (ch)
+            {
+                case '+':
+                    command = 1;
+                    break;
+                case '-':
+                    command = 2;
+                    break;
+                case '*':
+                    command = 3;
+                    break;
+                case '/':
+                    command = 4;
+                    break;
+            }
+
+            return command;
+        }
+
+        private static int KeyCodeToEditorCommand(Keys ch)
+        {
+            int command = 14;
+            switch (ch)
+            {
+                case Keys.Back:
+                    command = 12;
+                    break;
+                case Keys.Delete:
+                case Keys.Escape:
+                    command = 13;
+                    break;
+            }
+
+            return command;
+        }
+
+
+        private void AboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Калькулятор простых дробей\n\nВариант 13\nТип числа - простая дробь\nПрецеденты - 1-6\nОперанды могут браться из:\n  памяти - да\n  буфера обмена - да\nИстория - нет\nНастройки - нет\n\nРазработчик: Петровский В.Е., ИП-014", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void Button_Number_Edit(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int tag_command = Convert.ToInt32(button.Tag.ToString());
+            textBox1.Text = fracController.ExecComandEditor(tag_command);
+        }
+
+        private void Button_Number_Operation(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int tag_command = Convert.ToInt32(button.Tag.ToString());
+            textBox1.Text = fracController.ExecOperation(tag_command);
+        }
+
+        private void Button_Number_Function(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int tag_command = Convert.ToInt32(button.Tag.ToString());
+            textBox1.Text = fracController.ExecFunction(tag_command);
+        }
+
+        private void Button_Memory(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int tag_command = Convert.ToInt32(button.Tag.ToString());
+            dynamic exec = fracController.ExecCommandMemory(tag_command, textBox1.Text);
+            if (tag_command == 3)
+                textBox1.Text = exec.Item1.ToString();
+            label1.Text = exec.Item2 == true ? "M" : string.Empty;
+        }
+
+        private void Button_Reset(object sender, EventArgs e)
+        {
+            textBox1.Text = fracController.Reset();
+            label1.Text = string.Empty;
+        }
+
+        private void Button_Calculate(object sender, EventArgs e)
+        {
+            textBox1.Text = fracController.Calculate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+     
+            if (e.KeyCode == Keys.Enter)
+                CalculateButton.PerformClick();
+            else
+            {
+                int command = KeyCodeToEditorCommand(e.KeyCode);
+                if (command != 14)
+                    textBox1.Text = fracController.ExecComandEditor(command);
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                e.Handled = true;
+            if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '.')
+                textBox1.Text = fracController.ExecComandEditor(CharToEditorCommand(e.KeyChar));
+            else if (operation_signs.Contains(e.KeyChar))
+                textBox1.Text = fracController.ExecOperation(CharToOperationsCommand<TFrac>(e.KeyChar));
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show($"KeyUp code: {e.KeyCode}, value: {e.KeyValue}, modifiers: {e.Modifiers}" + "\r\n");
+            
+        }
+
+        private void SettingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(button4, "Очистить память");
+            toolTip.SetToolTip(button10, "Читать значение из памяти");
+            toolTip.SetToolTip(button11, "Сохранить значение в памяти");
+            toolTip.SetToolTip(button12, "Добавить к значению в памяти");
+        }
+    }
+}
---
+++ STP PART 2/RGZ_Petrovskiy/rgz/TFrac.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace rgz
+{
+    public class TFrac
+    {
+        private long numerator;
+        private long denominator;
+
+        public long Numerator
+        {
+            set
+            {
+                numerator = value;
+            }
+            get
+            {
+                return numerator;
+            }
+        }
+
+        public long Denominator
+        {
+            set
+            {
+                denominator = value;
+            }
+            get
+            {
+                return denominator;
+            }
+        }
+
+        static void Swap<T>(ref T var_1, ref T var_2)
+        {
+            T temp;
+            temp = var_1;
+            var_1 = var_2;
+            var_2 = temp;
+        }
+
+        public static long GCD(long a, long b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b > 0)
+            {
+                a %= b;
+                Swap(ref a, ref b);
+            }
+            return a;
+        }
+
+        public TFrac()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+
+        public TFrac(long a, long b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть равен нулю.");
+            }
+
+            long gcdRes = GCD(a, b);
+            numerator = a / gcdRes;
+            denominator = b / gcdRes;
+
+            if (denominator < 0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+        }
+
+        public TFrac(string frac)
+        {
+            Regex FracRegex = new Regex(@"^-?(\d+)/(\d+)$");
+            Regex NumberRegex = new Regex(@"^-?\d+/?$");
+            if (FracRegex.IsMatch(frac))
+            {
+                List<string> FracSplited = frac.Split('/').ToList();
+                numerator = Convert.ToInt64(FracSplited[0]);
+                denominator = Convert.ToInt64(FracSplited[1]);
+                if (denominator == 0)
+                {
+                    numerator = 0;
+                    denominator = 1;
+                    return;
+                }
+                long gcd = GCD(numerator, denominator);
+                if (gcd > 1)
+                {
+                    numerator /= gcd;
+                    denominator /= gcd;
+                }
+                return;
+            }
+            else if (NumberRegex.IsMatch(frac))
+            {
+                if (long.TryParse(frac, out long NewNumber))
+                    numerator = NewNumber;
+                else
+                    numerator = 0;
+                denominator = 1;
+                return;
+            }
+            else
+            {
+                numerator = 0;
+                denominator = 1;
+                return;
+            }
+        }
+
+        public TFrac Copy()
+        {
+            return (TFrac)this.MemberwiseClone();
+        }
+
+        public void SetString(string str)
+        {
+            TFrac TempFrac = new TFrac(str);
+            numerator = TempFrac.numerator;
+            denominator = TempFrac.denominator;
+        }
+
+        public TFrac Add(TFrac a)
+        {
+            return new TFrac(numerator * a.denominator + denominator * a.numerator, denominator * a.denominator);
+        }
+
+        public TFrac Mul(TFrac b)
+        {
+            return new TFrac(numerator * b.numerator, denominator * b.denominator);
+        }
+
+        public TFrac Sub(TFrac b)
+        {
+            return new TFrac(numerator * b.denominator - denominator * b.numerator, denominator * b.denominator);
+        }
+
+        public TFrac Div(TFrac b)
+        {
+            return new TFrac(numerator * b.denominator, denominator * b.numerator);
+        }
+
+        public TFrac Square()
+        {
+            return new TFrac(numerator * numerator, denominator * denominator);
+        }
+
+        public TFrac Reverse()
+        { 
+            return new TFrac(denominator, numerator); 
+        }
+
+        public TFrac Minus()
+        {
+            return new TFrac(-numerator, denominator);
+        }
+
+        public bool Equal(TFrac b)
+        {
+            return numerator == b.numerator && denominator == b.denominator;
+        }
+
+        public static bool operator >(TFrac a, TFrac b)
+        {
+            return (Convert.ToDouble(a.numerator) / Convert.ToDouble(a.denominator)) > (Convert.ToDouble(b.numerator) / Convert.ToDouble(b.denominator));
+        }
+
+        public static bool operator <(TFrac a, TFrac b)
+        {
+            return (Convert.ToDouble(a.numerator) / Convert.ToDouble(a.denominator)) < (Convert.ToDouble(b.numerator) / Convert.ToDouble(b.denominator));
+        }
+
+        public static implicit operator string(TFrac v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long getNumeratorNum()
+        {
+            return numerator;
+        }
+
+        public long getDenominatorNum()
+        {
+            return denominator;
+        }
+
+        public string getNumeratorString()
+        {
+            return numerator.ToString();
+        }
+
+        public string getDenominatorString()
+        {
+            return denominator.ToString();
+        }
+
+        public override string ToString()
+        {
+            return getNumeratorString() + "/" + getDenominatorString();
+        }
+    }
+}
